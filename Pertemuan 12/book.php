3076d86ab9c0d0aab3d88af88473ac628e85e0ba
MonDimaiss <dimas@MonDimaiss.com>
2025-05-23 11:33:02 +0700

    Tugas P12

---
+++ Pertemuan 12/book.php
@@ -0,0 +1,52 @@
+<?php
+require_once "method.php";
+
+$obj_book = new Book();
+$request_method = $_SERVER["REQUEST_METHOD"];
+
+switch ($request_method) {
+    case 'GET':
+        if (!empty($_GET["id"])) {
+            $id = intval($_GET["id"]);
+            $obj_book->get_book($id);
+        } else {
+            $obj_book->get_books();
+        }
+        break;
+
+    case 'POST':
+        if(!empty($_GET["id"])) {
+            $id = intval($_GET["id"]);
+            $obj_book->insert_book($id);
+        } else {
+            $obj_book->insert_book();
+        };
+        
+        break;
+
+    case 'DELETE':
+        if(empty($_GET["id"])) {
+            $response = [
+                'status' => 400,
+                'message' => 'Bad Request'
+            ];
+            header("Content-Type: application/json");
+            echo json_encode($response);
+        } else {
+            $id = intval($_GET["id"]);
+            $obj_book->delete_book($id);
+            $response = [
+                'status' => 200,
+                'message' => 'Book deleted successfully'
+            ];
+        }
+        break;
+        
+    default:
+        header("Content-Type: application/json");
+        echo json_encode([
+            'status' => 405,
+            'message' => 'Method Not Allowed'
+        ]);
+        break;
+}
